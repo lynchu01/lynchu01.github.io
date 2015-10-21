65b9136fd1160635b689d226309262f5ae4a4046
lynchu01 <hunter2814@yahoo.com>
2015-10-21 11:23:29 -0500

    Update todopage.js

--- todopage.js
+++ todopage.js
@@ -1,1 +1,25 @@
+addTodo = function() {
+  myUl = document.querySelector("#list");
+  myLi = document.createElement("li");
+  
+  myPriority = document.querySelector("#priority");
+  myLi.classList.add(myPriority.value);
+  
+  newIn = document.createElement("input");
+  newIn.type = "checkbox";
+  
+  myBox = document.querySelector("#input");
+  licontents = document.createTextNode(myBox.value);
+  
+  myLi.appendChild(newIn);
+  myLi.appendChild(licontents);
+  myUl.appendChild(myLi);
+  document.body.appendChild("#list");
+  
+}
 
+boxChecked = function() {
+  this.parentNode.classList.add("done");
+  mycb.onclick = boxChecked;
+  
+}
